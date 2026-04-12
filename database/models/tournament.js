c0bcfcc5349534841f0d171cfbe529bffb48a6b5
yuhdotpng <mcintirem11@gmail.com>
2026-04-12 14:32:51 -0500

    created seperate space for tournament model

---
+++ database/models/tournament.js
@@ -0,0 +1,46 @@
+const {  DataTypes } = require('sequelize');
+const db = require('../setup');
+
+// Tournament Model
+const Tournament = db.define('Tournament', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    entry_fee: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    attending_players: {
+        type: DataTypes.ARRAY(DataTypes.TEXT)
+    },
+    game_played: {
+        type: DataTypes.TEXT,
+        validate: {
+            isIN: ['original', 'melee', 'brawl', '4', 'ultimate', 'projectM']
+        },
+        allowNull: false
+    },
+    format: {
+        type: DataTypes.TEXT,
+        validate: {
+            isIN: ['single', 'double', 'round robin', 'pools', 'swiss']
+        },
+        allowNull: false
+    },
+    accept_reg: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true
+    }
+});
+
+module.exports = {Tournament};
