179413e9bfbb9bb08613ef3a8f232932df5ed0a6
yuhdotpng <mcintirem11@gmail.com>
2026-04-12 14:32:15 -0500

    Moved models to their own files

--- database/setup.js
+++ database/setup.js
@@ -8,115 +8,11 @@ const db = new Sequelize({
     logging: false
 });
 
-// User Model
-const User = db.define('User',{
-    id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    username:{
-        type: DataTypes.TEXT,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    location: {
-        type: DataTypes.TEXT,
-    },
-    role: {
-        type: DataTypes.TEXT,
-        defaultValue: 'user',
-        validate: {
-            isIN: ['user', 'player', 'TO', 'admin']
-        }
-    }
-});
+// Importing models
+const User = require('./models/user')(sequelize, DataTypes);
+const Player = require('./models/player')(sequelize, DataTypes);
+const Tournament = require('./models/tournament')(sequelize, DataTypes);
 
-// Players Model
-const Player = db.define('Player',{
-    id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    conference: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    main: {
-        type: DataTypes.ARRAY(DataTypes.TEXT),
-        allowNull: false
-    },
-    previous_rankings: {
-        type: DataTypes.ARRAY(DataTypes.INTEGER),
-        allowNull: false
-    },
-    season_ranking: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    status: {
-        type: DataTypes.TEXT,
-        defaultValue: 'inactive',
-        validate: {
-            isIN: ['active', 'inactive']
-        }
-    }
-});
-
-// Tournament Model
-const Tournament = db.define('Tournament', {
-    id:{
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    location: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    entry_fee: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-    },
-    attending_players: {
-        type: DataTypes.ARRAY(DataTypes.TEXT)
-    },
-    game_played: {
-        type: DataTypes.TEXT,
-        validate: {
-            isIN: ['original', 'melee', 'brawl', '4', 'ultimate', 'projectM']
-        },
-        allowNull: false
-    },
-    format: {
-        type: DataTypes.TEXT,
-        validate: {
-            isIN: ['single', 'double', 'round robin', 'pools', 'swiss']
-        },
-        allowNull: false
-    },
-    accept_reg: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-    }
-});
 //Defining player and user table relationship
 if (User.role === 'player'){
     User.hasOne(Player, {foreignKey: 'userId'});
@@ -134,7 +30,7 @@ if (User.role === 'TO') {
 };
 
 // Export for use in other files
-module.exports = { db, User, Player, Tournament };
+module.exports = db;
 
 // Create database and tables
 async function setupDatabase() {
