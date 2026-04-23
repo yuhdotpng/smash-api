dedb6f121b6387b7456610917a9ac5b279f2dd02
yuhdotpng <mcintirem11@gmail.com>
2026-04-23 17:05:58 -0500

    updated player model to allow it to be in it's own file and be imported into server.js

--- database/models/player.js
+++ database/models/player.js
@@ -1,44 +1,36 @@
 const { Sequelize, DataTypes } = require('sequelize');
-// Initialize database connection
-const db = new Sequelize({
-    dialect: 'sqlite',
-    storage: `database/${process.env.DB_NAME}` || 'smash.db',
-    logging: false
-}); 
+
 
 // Players Model
-const Player = db.define('Player',{
+const Player = {
     id:{
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
     name: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
     },
     conference: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
     },
     main: {
-        type: DataTypes.ARRAY(DataTypes.TEXT),
+        type: DataTypes.STRING,
         allowNull: false
     },
     previous_rankings: {
-        type: DataTypes.ARRAY(DataTypes.INTEGER),
+        type: DataTypes.JSON,
         allowNull: false
     },
     season_ranking: {
         type: DataTypes.INTEGER,
     },
-    status: {
-        type: DataTypes.TEXT,
-        defaultValue: 'inactive',
-        validate: {
-            isIN: ['active', 'inactive']
-        }
+    active_status: {
+        type: DataTypes. BOOLEAN,
+        defaultValue: false,
     }
-});
+};
 
 module.exports = Player;
